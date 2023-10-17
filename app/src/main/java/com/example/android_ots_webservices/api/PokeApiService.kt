2382b0b413d6ec09da04febea3267734268dbf2a
Luigi Longo <luigicl@gmail.com>
2023-10-16 22:32:12 -0300

    Melhorias visuais no card

--- app/src/main/java/com/example/android_ots_webservices/api/PokeApiService.kt
+++ app/src/main/java/com/example/android_ots_webservices/api/PokeApiService.kt
@@ -1,6 +1,6 @@
 package com.example.android_ots_webservices.api
 
-import com.example.android_ots_webservices.api.ListPokemonApiResult
+// import com.example.android_ots_webservices.api.ListPokemonApiResult
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -8,6 +8,6 @@ interface PokeApiService {
     // https://pokeapi.co/api/v2/pokemon?limit=20&offset=0
     // Base: https://pokeapi.co/api/v2/
     // Endpoint (Rota): pokemon?limit=20&offset=0
-    @GET("pokemon?limit=20&offset=0")
+    @GET("pokemon?limit=10000&offset=0")
     fun listPokemon(): Call<ListPokemonApiResult>
 }
--- app/src/main/java/com/example/android_ots_webservices/list/PokemonListAdapter.kt
+++ app/src/main/java/com/example/android_ots_webservices/list/PokemonListAdapter.kt
@@ -42,7 +42,9 @@ class PokemonListAdapter(private val dataSet: Array<PokemonItem>) :
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         val pokemonItem = dataSet[position]
-        viewHolder.tvName.text = pokemonItem.name
+        viewHolder.tvName.text = pokemonItem.name.replaceFirstChar {
+            it.uppercase()
+        }
         Glide.with(viewHolder.ivImage)
             .load(pokemonItem.imageUrl)
             .into(viewHolder.ivImage)
