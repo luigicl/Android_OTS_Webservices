3c477c6f2f9e118f3d374d07f9d085159fe9a362
Luigi Longo <luigicl@gmail.com>
2023-10-16 21:55:22 -0300

    Carregando dados da API

--- app/src/main/java/com/example/android_ots_webservices/MainActivity.kt
+++ app/src/main/java/com/example/android_ots_webservices/MainActivity.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import retrofit2.Call
 import retrofit2.Callback
@@ -41,10 +43,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // carregamento de imagem com Glide
-        val ivImage = findViewById<ImageView>(R.id.ivImage)
-        Glide.with(ivImage)
-            .load("https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png")
-            .into(ivImage)
+//         val ivImage = findViewById<ImageView>(R.id.ivImage)
+//         Glide.with(ivImage)
+//             .load("https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png")
+//             .into(ivImage)
 
         // Carregamento do Retrofit
         val retrofit = Retrofit.Builder()
@@ -67,11 +69,27 @@ class MainActivity : AppCompatActivity() {
                 val tvName = findViewById<TextView>(R.id.tvName)
 
                 response.body()?.let {
-                    tvName.text = ""
+                    // TODO: Transformar lista de resultados da API em lista de itens do Adapter
+                    // TODO: Carregar o RecyclerView com os itens do Adapter
 
-                    it.results.forEach { pokemon ->
-                        tvName.append(pokemon.name + "\n") // \n quebra de linha
-                    }
+//                    val pokemon1 = PokemonItem("Nome 001", "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png")
+//                    val pokemon2 = PokemonItem("Nome 002", "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png")
+//                    val pokemonItems = arrayOf(pokemon1, pokemon2)
+
+                    val pokemonItems = it.results.mapIndexed { index, result ->
+                        val number = (index + 1).toString().padStart(3, '0') // "" indica string, '' indica caractere
+                        PokemonItem(result.name,
+                            "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/$number.png"
+                        )
+                    }.toTypedArray()
+
+                    val rvPokemon = findViewById<RecyclerView>(R.id.rvPokemon)
+                    rvPokemon.layoutManager = LinearLayoutManager(this@MainActivity)
+                    rvPokemon.adapter = PokemonListAdapter(pokemonItems)
+//                    tvName.text = ""
+//                    it.results.forEach { pokemon ->
+//                        tvName.append(pokemon.name + "\n") // \n quebra de linha
+//                    }
                 }
             }
 
