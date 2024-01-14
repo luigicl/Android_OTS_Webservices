5c6bd524d6d3731aa2c2579b2e00f100fb1ae89a
Luigi Longo <luigicl@gmail.com>
2024-01-13 21:31:58 -0300

    iniciando a implementação de nova tela para detalhes

---
+++ app/src/main/java/com/example/android_ots_webservices/view/PokemonViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android_ots_webservices.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_ots_webservices.R
+
+class PokemonViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_view)
+    }
+}
